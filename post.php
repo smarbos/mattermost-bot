888025ce05a3ad52bdee5845fceeeb0620ce6ab5
Damian Mantuano <damianm@camba.coop>
2016-11-05 20:00:26 -0300

    added fallback for hash_equals

--- post.php
+++ post.php
@@ -2,6 +2,26 @@
 
     ini_set('error_reporting', E_ALL);
     require_once('users.php');
+    if(!function_exists('hash_equals'))
+    {
+        function hash_equals($str1, $str2)
+        {
+            if(strlen($str1) != strlen($str2))
+            {
+                return false;
+            }
+            else
+            {
+                $res = $str1 ^ $str2;
+                $ret = 0;
+                for($i = strlen($res) - 1; $i >= 0; $i--)
+                {
+                    $ret |= ord($res[$i]);
+                }
+                return !$ret;
+            }
+        }
+    }
     
     function enviarMensaje($message){
         $ch = curl_init();
