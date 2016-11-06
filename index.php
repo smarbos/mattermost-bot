65e086884b3ea48ee57a6a750c5d1668ea66cb24
Damian Mantuano <damianm@camba.coop>
2016-11-06 15:05:53 -0300

    Change url on load, added bad password error

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
             <div class="col-sm-12">
                 <?php if($_GET['status'] == 'ok'){ echo "<span class=\"status status-ok\">OK</span>"; } ?>
                 <?php if($_GET['status'] == 'fail'){ echo "<span class=\"status status-fail\">FAIL</span>"; } ?>
+                <?php if($_GET['status'] == 'bad-pswd'){ echo "<span class=\"status status-bad-pswd\"><span class=\"status-content\">WTF?</span></span>"; } ?>
                 <form class="" action="post.php" method="post">
                     <div class="row">
                         <div class="col-sm-12">
@@ -36,6 +37,10 @@
 </html>
 <script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
 <script type="text/javascript">
+
+/*
+    Cambia el foco del input cuando el usuario presiona la tecla enter.
+*/
 $('.inputs').keyup(function (e) {
      if (e.which === 13) {
          var index = $('.inputs').index(this) + 1;
@@ -43,10 +48,42 @@ $('.inputs').keyup(function (e) {
      }
  });
 
- $( document ).ready(function() {
-     $( ".status" ).fadeOut( 6000, function() {
-       // Animation complete
-     });
- });
+ /*
+     Fade out de los status cuando termina de cargar.
+ */
+ var something = (function() {
+    var executed = false;
+    return function () {
+        if (!executed) {
+            executed = true;
+            // do something
+        }
+    };
+})();
+
+$( document ).ready(function() {
+    $( ".status" ).fadeOut( 6000, function() {
+        if(window.location.href != url_parts[0])
+        {
+            var oldURL = window.location.href;
+            var index = 0;
+            var newURL = oldURL;
+            index = oldURL.indexOf('?');
+            if(index == -1){
+                index = oldURL.indexOf('#');
+            }
+            if(index != -1){
+                newURL = oldURL.substring(0, index);
+            }
+            window.location.href = newURL;
+        }
+
+});
+
+var url = window.location.href;
+url_parts = url.split("?");
+console.log(url_parts[0]);
+
+});
 
 </script>
